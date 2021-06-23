360e0a3f32b5150bca431474020d414507530f80
HT <hoangthanh@gxd.vn>
2021-06-23 02:46:31 -0700

    23/06

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/dist/" : "/",
+    process.env.NODE_ENV === "production" ? "/dist" : "/",
   configureWebpack: {
     resolve: {
       alias: {
