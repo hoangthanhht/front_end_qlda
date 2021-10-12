ca667f99c82200867b40a1b2b9232cd4b18004ab
HT <hoangthanh@gxd.vn>
2021-10-12 17:45:35 +0700

    chuyen server

--- src/core/services/plugins/axios.js
+++ src/core/services/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-    baseURL: 'http://vue.gxd.vn/api/',
+    baseURL: 'https://api.giaxaydung.com/api/',
     //baseURL: 'http://127.0.0.1:8000/api/',
     headers: {'Content-Type': 'application/json',
               //'Access-Control-Allow-Origin': '*',
